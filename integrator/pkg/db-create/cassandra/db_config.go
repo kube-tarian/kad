8a01fac88f5a9ea8ba0b95ba9021503b69e9bf9f
share2kanna <ganeshkl@intelops.dev>
2022-12-15 23:57:24 +0530

    added cassandra create and migrate packages. It will take care of parallel pods scenario where replicas > 1.

---
+++ integrator/pkg/db-create/cassandra/db_config.go
@@ -0,0 +1,21 @@
+// Package cassandra contains ...
+package cassandra
+
+type DBConfig struct {
+	DbAddresses         []string `envconfig:"DB_ADDRESSES" required:"true"`
+	DbAdminUsername     string   `envconfig:"DB_ADMIN_USERNAME" required:"true"`
+	DbServiceUsername   string   `envconfig:"DB_SERVICE_USERNAME" required:"true"`
+	DbName              string   `envconfig:"CASSANDRA_DB_NAME" required:"true"`
+	DbReplicationFactor string   `envconfig:"DB_REPLICATION_FACTOR" required:"true"`
+	DbAdminPassword     string   `envconfig:"DB_ADMIN_PASSWD" required:"true"`
+	DbServicePassword   string   `envconfig:"DB_SERVICE_PASSWD" required:"true"`
+}
+
+type Store interface {
+	Connect(dbAddrs []string, dbAdminUsername string, dbAdminPassword string) (err error)
+	Close()
+	CreateDbUser(serviceUsername string, servicePassword string) (err error)
+	GrantPermission(serviceUsername string, dbName string) (err error)
+	CreateDb(dbName string, replicationFactor string) (err error)
+	CreateLockSchemaDb(replicationFactor string) (err error)
+}
