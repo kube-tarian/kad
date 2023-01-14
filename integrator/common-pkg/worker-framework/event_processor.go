2f3410e1dd7ebb98a3f91038ea0c09f33d1357d9
share2kanna <ganeshkl@intelops.dev>
2023-01-14 13:35:45 +0530

    Temporal namespace creation and verifying the creation

--- integrator/common-pkg/worker-framework/event_processor.go
+++ integrator/common-pkg/worker-framework/event_processor.go
@@ -1,18 +1,13 @@
 package workerframework
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"strings"
-	"time"
 
 	"github.com/kelseyhightower/envconfig"
 	"github.com/kube-tarian/kad/integrator/common-pkg/logging"
 	"github.com/kube-tarian/kad/integrator/common-pkg/temporalclient"
 
-	"go.temporal.io/api/workflowservice/v1"
-	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 )
 
@@ -85,33 +80,10 @@ func (w *Worker) RegisterToTemporal(taskQueueName string, wf, activity interface
 		return fmt.Errorf("unable to create client, %v", err)
 	}
 
-	// Below code is to register namespace
-	// Reference: https://docs.temporal.io/application-development/features?lang=go#namespaces
-	// Equivalent cli command: tctl --ns default namespace register -rd 3
-	// Reference: https://docs.temporal.io/tctl-v1/namespace#register
-	client, err := client.NewNamespaceClient(client.Options{HostPort: w.conf.TemporalServiceAddress})
+	err = w.temporalClient.RegisterNamespace()
 	if err != nil {
-		return fmt.Errorf("unable to create namespace client, %v", err)
+		return fmt.Errorf("default namespace create verification failed, %v", err)
 	}
-
-	var retention time.Duration = 3 * 24 * time.Hour
-	err = client.Register(context.Background(), &workflowservice.RegisterNamespaceRequest{
-		Namespace:                        "default",
-		WorkflowExecutionRetentionPeriod: &retention,
-	})
-	if err != nil && !strings.Contains(err.Error(), "Namespace already exists") {
-		return fmt.Errorf("unable to register namesapce, %v", err)
-	}
-	w.logger.Infof("default namespace registered. Verifying whether reflected or not")
-	for i := 0; i < 3; i++ {
-		_, err = client.Describe(context.Background(), "default")
-		if err != nil {
-			w.logger.Errorf("retrying, namesapce not found, %v", err)
-			continue
-		}
-		break
-	}
-	w.logger.Infof("default namespace registered and verified")
 
 	w.temporalWorker = worker.New(w.temporalClient.TemporalClient, taskQueueName, worker.Options{})
 	w.temporalWorker.RegisterWorkflow(wf)
