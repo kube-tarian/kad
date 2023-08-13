0675f773d169889521bb8a09b27054c3e5ae0871
anil-intelops <anil@intelops.dev>
2023-08-13 19:09:19 +0530

    create app config table is added to init job

--- server/pkg/store/astra/store.go
+++ server/pkg/store/astra/store.go
@@ -31,6 +31,7 @@ func (a *AstraServerStore) initClient() error {
 func (a *AstraServerStore) InitializeDb() error {
 	initDbQueries := []string{
 		fmt.Sprintf(createClusterEndpointTableQuery, a.keyspace),
+		fmt.Sprintf(createAppConfigTableQuery, a.keyspace),
 	}
 
 	for _, query := range initDbQueries {
