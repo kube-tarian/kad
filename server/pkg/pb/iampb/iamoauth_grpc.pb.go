1a5e33801ed6680b75f34e54e12b1d319d8581a8
vijeyash <vijeyash@gmail.com>
2023-08-18 14:47:06 +0530

    new oauth iam proto code generated

--- server/pkg/pb/iampb/iamoauth_grpc.pb.go
+++ server/pkg/pb/iampb/iamoauth_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.21.12
-// source: iamoauth.proto
+// source: proto/oauthproto/oauth.proto
 
 package oauthproto
 
@@ -24,6 +24,8 @@ const _ = grpc.SupportPackageIsVersion7
 type OauthServiceClient interface {
 	CreateOauthClient(ctx context.Context, in *OauthClientRequest, opts ...grpc.CallOption) (*OauthClientResponse, error)
 	GetOauthToken(ctx context.Context, in *OauthTokenRequest, opts ...grpc.CallOption) (*OauthTokenResponse, error)
+	ValidateOauthToken(ctx context.Context, in *ValidateOauthTokenRequest, opts ...grpc.CallOption) (*ValidateOauthTokenResponse, error)
+	CreateClientCredentialsClient(ctx context.Context, in *CreateClientCredentialsClientRequest, opts ...grpc.CallOption) (*CreateClientCredentialsClientResponse, error)
 }
 
 type oauthServiceClient struct {
@@ -52,12 +54,32 @@ func (c *oauthServiceClient) GetOauthToken(ctx context.Context, in *OauthTokenRe
 	return out, nil
 }
 
+func (c *oauthServiceClient) ValidateOauthToken(ctx context.Context, in *ValidateOauthTokenRequest, opts ...grpc.CallOption) (*ValidateOauthTokenResponse, error) {
+	out := new(ValidateOauthTokenResponse)
+	err := c.cc.Invoke(ctx, "/OauthService.OauthService/ValidateOauthToken", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *oauthServiceClient) CreateClientCredentialsClient(ctx context.Context, in *CreateClientCredentialsClientRequest, opts ...grpc.CallOption) (*CreateClientCredentialsClientResponse, error) {
+	out := new(CreateClientCredentialsClientResponse)
+	err := c.cc.Invoke(ctx, "/OauthService.OauthService/CreateClientCredentialsClient", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // OauthServiceServer is the server API for OauthService service.
 // All implementations must embed UnimplementedOauthServiceServer
 // for forward compatibility
 type OauthServiceServer interface {
 	CreateOauthClient(context.Context, *OauthClientRequest) (*OauthClientResponse, error)
 	GetOauthToken(context.Context, *OauthTokenRequest) (*OauthTokenResponse, error)
+	ValidateOauthToken(context.Context, *ValidateOauthTokenRequest) (*ValidateOauthTokenResponse, error)
+	CreateClientCredentialsClient(context.Context, *CreateClientCredentialsClientRequest) (*CreateClientCredentialsClientResponse, error)
 	mustEmbedUnimplementedOauthServiceServer()
 }
 
@@ -70,6 +92,12 @@ func (UnimplementedOauthServiceServer) CreateOauthClient(context.Context, *Oauth
 }
 func (UnimplementedOauthServiceServer) GetOauthToken(context.Context, *OauthTokenRequest) (*OauthTokenResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetOauthToken not implemented")
+}
+func (UnimplementedOauthServiceServer) ValidateOauthToken(context.Context, *ValidateOauthTokenRequest) (*ValidateOauthTokenResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ValidateOauthToken not implemented")
+}
+func (UnimplementedOauthServiceServer) CreateClientCredentialsClient(context.Context, *CreateClientCredentialsClientRequest) (*CreateClientCredentialsClientResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateClientCredentialsClient not implemented")
 }
 func (UnimplementedOauthServiceServer) mustEmbedUnimplementedOauthServiceServer() {}
 
@@ -120,6 +148,42 @@ func _OauthService_GetOauthToken_Handler(srv interface{}, ctx context.Context, d
 	return interceptor(ctx, in, info, handler)
 }
 
+func _OauthService_ValidateOauthToken_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ValidateOauthTokenRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(OauthServiceServer).ValidateOauthToken(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/OauthService.OauthService/ValidateOauthToken",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(OauthServiceServer).ValidateOauthToken(ctx, req.(*ValidateOauthTokenRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _OauthService_CreateClientCredentialsClient_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreateClientCredentialsClientRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(OauthServiceServer).CreateClientCredentialsClient(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/OauthService.OauthService/CreateClientCredentialsClient",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(OauthServiceServer).CreateClientCredentialsClient(ctx, req.(*CreateClientCredentialsClientRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // OauthService_ServiceDesc is the grpc.ServiceDesc for OauthService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -135,7 +199,15 @@ var OauthService_ServiceDesc = grpc.ServiceDesc{
 			MethodName: "GetOauthToken",
 			Handler:    _OauthService_GetOauthToken_Handler,
 		},
+		{
+			MethodName: "ValidateOauthToken",
+			Handler:    _OauthService_ValidateOauthToken_Handler,
+		},
+		{
+			MethodName: "CreateClientCredentialsClient",
+			Handler:    _OauthService_CreateClientCredentialsClient_Handler,
+		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "iamoauth.proto",
+	Metadata: "proto/oauthproto/oauth.proto",
 }
