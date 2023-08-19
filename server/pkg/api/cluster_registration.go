ea45eb86fd3a0f8d6370c7ef75c9041663576979
indresh-28 <indresh@intelops.dev>
2023-08-19 12:17:53 +0530

    remove unnecessary comments

--- server/pkg/api/cluster_registration.go
+++ server/pkg/api/cluster_registration.go
@@ -70,13 +70,6 @@ func (s *Server) NewClusterRegistration(ctx context.Context, request *serverpb.N
 		}, nil
 	}
 
-	// Get installed clusterApps/LaunchUI. check if launchUI config is there then do all these.
-	// Call the IAM module to get the client ID and secret for that APP.
-	// Store the secrets on Agent Vault.
-	// trigger configureAgentSSO.
-	// Do all these on clusterUpdate, Cleanup all on the cluster Delete.?
-	// during App addon we need to perform these actions.
-	// Usually this process takes longer so UI need to enhance to show this behavior..
 	a, err := s.agentHandeler.GetAgent(orgId, clusterID)
 	if err != nil {
 		s.log.Error("failed to connect to agent", err)
