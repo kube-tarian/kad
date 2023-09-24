2dd77675861b9d36e60e78c1ba11151a74f5f33e
indresh-28 <indresh@intelops.dev>
2023-09-24 20:12:45 +0530

    update activity name

--- capten/config-worker/pkg/activities/activity.go
+++ capten/config-worker/pkg/activities/activity.go
@@ -15,7 +15,7 @@ type Activities struct {
 var logger = logging.NewLogger()
 
 func (a *Activities) ConfigurationActivity(ctx context.Context, params model.ConfigureParameters, payload json.RawMessage) (model.ResponsePayload, error) {
-	logger.Infof("Activity, name: %+v", payload)
+	logger.Infof("Activity, name: %+v", params.Resource)
 	// e := activity.GetInfo(ctx)
 	// logger.Infof("activity info: %+v", e)
 
