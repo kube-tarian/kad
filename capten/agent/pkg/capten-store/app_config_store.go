c5bcaef0376877211e9c814918c953a275e87f55
anil-intelops <anil@intelops.dev>
2023-09-24 21:00:07 +0530

    onboarding integrations

--- capten/agent/pkg/capten-store/app_config_store.go
+++ capten/agent/pkg/capten-store/app_config_store.go
@@ -308,9 +308,15 @@ func (a *Store) DeleteOnboardingIntegration(usecase, onboardingProjectUrl string
 	deleteQuery := a.client.Session().Query(fmt.Sprintf(deleteOnboardingIntegrationQuery,
 		a.keyspace, usecase, onboardingProjectUrl))
 
-	if err := deleteQuery.Exec(); err != nil {
+	fmt.Println(fmt.Sprintf(deleteOnboardingIntegrationQuery,
+		a.keyspace, usecase, onboardingProjectUrl))
+
+	err := deleteQuery.Exec()
+	if err != nil {
 		return err
 	}
+
+	fmt.Println(fmt.Sprintf("Error - %v", err))
 
 	return nil
 }
