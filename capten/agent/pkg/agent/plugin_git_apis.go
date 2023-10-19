1cd82d7c192aac5165dd3d5124889151f566439f
sol-dev-abhi <sol.dev.abhi@gmail.com>
2023-10-19 20:56:27 +0530

    added capten gitProjects implementation

--- capten/agent/pkg/agent/plugin_git_apis.go
+++ capten/agent/pkg/agent/plugin_git_apis.go
@@ -4,45 +4,226 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/uuid"
+	"github.com/intelops/go-common/credentials"
 	"github.com/kube-tarian/kad/capten/agent/pkg/pb/captenpluginspb"
 )
 
+const gitProjectEntityName = "gitproject"
+
 func (a *Agent) AddGitProject(ctx context.Context, request *captenpluginspb.AddGitProjectRequest) (
 	*captenpluginspb.AddGitProjectResponse, error) {
+
+	// create new id
+	id := uuid.New()
+
+	// save to vault
+	if err := a.storeAccesToken(ctx, id.String(), request.AccessToken); err != nil {
+		return &captenpluginspb.AddGitProjectResponse{
+			Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+			StatusMessage: err.Error(),
+		}, nil
+	}
+
+	// save to db
+	gitProject := captenpluginspb.GitProject{
+		Id:             id.String(),
+		ProjectUrl:     request.ProjectUrl,
+		Labels:         request.Labels,
+		LastUpdateTime: request.LastUpdateTime,
+	}
+	if err := a.as.UpsertGitProject(&gitProject); err != nil {
+		a.log.Errorf("failed to add gitProject in db, %v", err)
+		return &captenpluginspb.AddGitProjectResponse{
+			Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+			StatusMessage: "failed to add gitProject in db",
+		}, nil
+	}
+
 	return &captenpluginspb.AddGitProjectResponse{
-		Status:        captenpluginspb.StatusCode_NOT_FOUND,
-		StatusMessage: "not implemented",
-	}, fmt.Errorf("not implemented")
+		Id:            id.String(),
+		Status:        captenpluginspb.StatusCode_OK,
+		StatusMessage: "ok",
+	}, nil
 }
 
 func (a *Agent) UpdateGitProject(ctx context.Context, request *captenpluginspb.UpdateGitProjectRequest) (
 	*captenpluginspb.UpdateGitProjectResponse, error) {
+
+	// validate id
+	id, err := uuid.Parse(request.Id)
+	if err != nil {
+		return &captenpluginspb.UpdateGitProjectResponse{
+			Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+			StatusMessage: fmt.Sprintf("invalid uuid: %s", request.Id),
+		}, nil
+	}
+
+	if request.AccessToken != "" {
+		// save to vault
+		if err := a.storeAccesToken(ctx, request.Id, request.AccessToken); err != nil {
+			return &captenpluginspb.UpdateGitProjectResponse{
+				Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+				StatusMessage: err.Error(),
+			}, nil
+		}
+	}
+
+	// save to db
+	gitProject := captenpluginspb.GitProject{
+		Id:             id.String(),
+		ProjectUrl:     request.ProjectUrl,
+		Labels:         request.Labels,
+		LastUpdateTime: request.LastUpdateTime,
+	}
+	if err := a.as.UpsertGitProject(&gitProject); err != nil {
+		a.log.Errorf("failed to update gitProject in db, %v", err)
+		return &captenpluginspb.UpdateGitProjectResponse{
+			Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+			StatusMessage: "failed to update gitProject in db",
+		}, nil
+	}
+
 	return &captenpluginspb.UpdateGitProjectResponse{
-		Status:        captenpluginspb.StatusCode_NOT_FOUND,
-		StatusMessage: "not implemented",
-	}, fmt.Errorf("not implemented")
+		Status:        captenpluginspb.StatusCode_OK,
+		StatusMessage: "ok",
+	}, nil
 }
 
 func (a *Agent) DeleteGitProject(ctx context.Context, request *captenpluginspb.DeleteGitProjectRequest) (
 	*captenpluginspb.DeleteGitProjectResponse, error) {
+	if err := a.as.DeleteGitProjectById(request.Id); err != nil {
+		a.log.Errorf("failed to delete gitProject from db, %v", err)
+		return &captenpluginspb.DeleteGitProjectResponse{
+			Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+			StatusMessage: "failed to delete gitProject from db",
+		}, nil
+	}
+
 	return &captenpluginspb.DeleteGitProjectResponse{
-		Status:        captenpluginspb.StatusCode_NOT_FOUND,
-		StatusMessage: "not implemented",
-	}, fmt.Errorf("not implemented")
+		Status:        captenpluginspb.StatusCode_OK,
+		StatusMessage: "ok",
+	}, nil
 }
 
 func (a *Agent) GetGitProjects(ctx context.Context, request *captenpluginspb.GetGitProjectsRequest) (
 	*captenpluginspb.GetGitProjectsResponse, error) {
+
+	res, err := a.as.GetGitProjects()
+	if err != nil {
+		return &captenpluginspb.GetGitProjectsResponse{
+			Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+			StatusMessage: "failed to fetch git projects",
+		}, nil
+	}
+
+	for _, r := range res {
+		accessToken, err := a.getAccesToken(ctx, r.Id)
+		if err != nil {
+			return &captenpluginspb.GetGitProjectsResponse{
+				Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+				StatusMessage: "failed to fetch git projects",
+			}, nil
+		}
+		r.AccessToken = accessToken
+	}
+
+	a.log.Infof("Found %d gitProjects", len(res))
 	return &captenpluginspb.GetGitProjectsResponse{
-		Status:        captenpluginspb.StatusCode_NOT_FOUND,
-		StatusMessage: "not implemented",
-	}, fmt.Errorf("not implemented")
+		Status:        captenpluginspb.StatusCode_OK,
+		StatusMessage: "successful",
+		GitProjects:   res,
+	}, nil
+
 }
 
 func (a *Agent) GetGitProjectsForLabels(ctx context.Context, request *captenpluginspb.GetGitProjectsForLabelsRequest) (
 	*captenpluginspb.GetGitProjectsForLabelsResponse, error) {
+
+	res, err := a.as.GetGitProjects()
+	if err != nil {
+		return &captenpluginspb.GetGitProjectsForLabelsResponse{
+			Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+			StatusMessage: "failed to fetch git projects",
+		}, nil
+	}
+
+	filteredRes := make([]*captenpluginspb.GitProject, 0)
+	for _, r := range res {
+		if atleastOneLabelsMatch(request.Labels, r.Labels) {
+			// fetch cred
+			accessToken, err := a.getAccesToken(ctx, r.Id)
+			if err != nil {
+				return &captenpluginspb.GetGitProjectsForLabelsResponse{
+					Status:        captenpluginspb.StatusCode_INTERNAL_ERROR,
+					StatusMessage: "failed to fetch git projects",
+				}, nil
+			}
+			filteredRes = append(filteredRes, &captenpluginspb.GitProject{
+				Id:          r.Id,
+				ProjectUrl:  r.ProjectUrl,
+				Labels:      r.Labels,
+				AccessToken: accessToken,
+			})
+		}
+	}
+
 	return &captenpluginspb.GetGitProjectsForLabelsResponse{
-		Status:        captenpluginspb.StatusCode_NOT_FOUND,
-		StatusMessage: "not implemented",
-	}, fmt.Errorf("not implemented")
+		Status:        captenpluginspb.StatusCode_OK,
+		StatusMessage: "successful",
+		Projects:      filteredRes,
+	}, nil
+}
+
+func (a *Agent) getAccesToken(ctx context.Context, id string) (string, error) {
+	credPath := fmt.Sprintf("%s/%s/%s", credentials.GenericCredentialType, gitProjectEntityName, id)
+	credAdmin, err := credentials.NewCredentialAdmin(ctx)
+	if err != nil {
+		a.log.Audit("security", "storecred", "failed", "system", "failed to intialize credentials client for %s", credPath)
+		a.log.Errorf("failed to get crendential for %s, %v", credPath, err)
+		return "", err
+	}
+
+	cred, err := credAdmin.GetCredential(ctx, credentials.GenericCredentialType, gitProjectEntityName, id)
+	if err != nil {
+		a.log.Errorf("failed to get credential for %s, %v", credPath, err)
+		return "", err
+	}
+	return cred["accessToken"], nil
+}
+
+func (a *Agent) storeAccesToken(ctx context.Context, id string, accessToken string) error {
+	credPath := fmt.Sprintf("%s/%s/%s", credentials.GenericCredentialType, gitProjectEntityName, id)
+	credAdmin, err := credentials.NewCredentialAdmin(ctx)
+	if err != nil {
+		a.log.Audit("security", "storecred", "failed", "system", "failed to intialize credentials client for %s", credPath)
+		a.log.Errorf("failed to store credential for %s, %v", credPath, err)
+		return err
+	}
+
+	credentialMap := map[string]string{
+		"accessToken": accessToken,
+	}
+	err = credAdmin.PutCredential(ctx, credentials.GenericCredentialType, gitProjectEntityName,
+		id, credentialMap)
+
+	if err != nil {
+		a.log.Audit("security", "storecred", "failed", "system", "failed to store crendential for %s", credPath)
+		a.log.Errorf("failed to store credential for %s, %v", credPath, err)
+		return err
+	}
+	a.log.Audit("security", "storecred", "success", "system", "credential stored for %s", credPath)
+	a.log.Infof("stored credential for entity %s", credPath)
+	return nil
+}
+
+func atleastOneLabelsMatch(arr1, arr2 []string) bool {
+	for _, key1 := range arr1 {
+		for _, key2 := range arr2 {
+			if key1 == key2 {
+				return true
+			}
+		}
+	}
+	return false
 }
---
+++ capten/agent/pkg/capten-store/git_projects.go
@@ -0,0 +1,98 @@
+package captenstore
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gocql/gocql"
+	"github.com/kube-tarian/kad/capten/agent/pkg/pb/captenpluginspb"
+	"github.com/pkg/errors"
+)
+
+const (
+	insertGitProject     = "INSERT INTO %s.GitProjects(id, project_url, labels, last_update_time) VALUES (?)"
+	insertGitProjectId   = "INSERT INTO %s.GitProjects(id) VALUES (?)"
+	updateGitProjectById = "UPDATE %s.GitProjects SET %s WHERE id = ?"
+	deleteGitProjectById = "DELETE FROM %s.GitProjects WHERE id= ?"
+)
+
+func (a *Store) UpsertGitProject(config *captenpluginspb.GitProject) error {
+
+	kvPairs, isEmptyUpdate := formUpdateKvPairsForGitProject(config)
+	batch := a.client.Session().NewBatch(gocql.LoggedBatch)
+	batch.Query(fmt.Sprintf(insertGitProjectId, a.keyspace), config.Id)
+	if !isEmptyUpdate {
+		batch.Query(fmt.Sprintf(updateGitProjectById, a.keyspace, kvPairs), config.Id)
+	}
+	return a.client.Session().ExecuteBatch(batch)
+}
+
+func (a *Store) DeleteGitProjectById(id string) error {
+
+	deleteAction := a.client.Session().Query(fmt.Sprintf(deleteGitProjectById,
+		a.keyspace), id)
+
+	err := deleteAction.Exec()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *Store) GetGitProjects() ([]*captenpluginspb.GitProject, error) {
+
+	query := fmt.Sprintf("SELECT id, project_url, labels, last_update_time FROM %s.GitProjects", a.keyspace)
+
+	selectQuery := a.client.Session().Query(query)
+	iter := selectQuery.Iter()
+
+	config := captenpluginspb.GitProject{}
+	var labelsAll string
+
+	ret := make([]*captenpluginspb.GitProject, 0)
+	for iter.Scan(
+		&config.Id, &config.ProjectUrl,
+		&labelsAll, &config.LastUpdateTime,
+	) {
+		gitProject := &captenpluginspb.GitProject{
+			Id:             config.Id,
+			ProjectUrl:     config.ProjectUrl,
+			Labels:         strings.Split(labelsAll, ","),
+			LastUpdateTime: config.LastUpdateTime,
+		}
+		ret = append(ret, gitProject)
+	}
+
+	if err := iter.Close(); err != nil {
+		return nil, errors.WithMessage(err, "failed to iterate through results:")
+	}
+
+	return ret, nil
+}
+
+func formUpdateKvPairsForGitProject(config *captenpluginspb.GitProject) (kvPairs string, emptyUpdate bool) {
+	params := []string{}
+
+	if config.ProjectUrl != "" {
+		params = append(params,
+			fmt.Sprintf("project_url = '%s'", config.ProjectUrl))
+	}
+
+	// comma separated labels, change this later
+	if len(config.Labels) > 0 {
+		params = append(params,
+			fmt.Sprintf("labels = '%v'", strings.Join(config.Labels, ",")))
+	}
+
+	if (config.LastUpdateTime) != "" {
+		params = append(params,
+			fmt.Sprintf("last_update_time = '%v'", config.LastUpdateTime))
+	}
+
+	if len(params) == 0 {
+		// query is empty there is nothing to update
+		return "", true
+	}
+	return strings.Join(params, ", "), false
+}
