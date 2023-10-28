8deee5e5b43af7f68a776c81a0ac2705206e6600
sol-dev-abhi <sol.dev.abhi@gmail.com>
2023-10-28 21:48:20 +0530

    removed logs

--- capten/agent/pkg/capten-store/cloud_provider.go
+++ capten/agent/pkg/capten-store/cloud_provider.go
@@ -27,10 +27,8 @@ func (a *Store) UpsertCloudProvider(config *captenpluginspb.CloudProvider) error
 	batch.Query(fmt.Sprintf(insertCloudProvider, a.keyspace), config.Id, config.CloudType, config.Labels, config.LastUpdateTime)
 	err := a.client.Session().ExecuteBatch(batch)
 	if err != nil {
-		fmt.Println("Err While inserting Cloud provider", err)
 		batch.Query(fmt.Sprintf(updateCloudProviderById, a.keyspace, kvPairs), config.Id)
 		err = a.client.Session().ExecuteBatch(batch)
-		fmt.Println("Err While updating cloud provider", err)
 	}
 	return err
 }
@@ -83,7 +81,6 @@ func (a *Store) GetCloudProvidersByLabelsAndCloudType(searchLabels []string, clo
 	}
 
 	query := fmt.Sprintf(selectAllCloudProvidersByLabels, a.keyspace, whereLabelsClause)
-	fmt.Println("QUERY for GetCloudProvidersByLabelsAndCloudType", query)
 	return a.executeCloudProvidersSelectQuery(query)
 
 }
