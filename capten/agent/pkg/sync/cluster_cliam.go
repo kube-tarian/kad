feae1e34bf61c631aa7ec089dfbc0f92d00a42f4
indresh-28 <indresh@intelops.dev>
2023-11-07 15:08:15 +0530

    get the secrets

--- capten/agent/pkg/sync/cluster_cliam.go
+++ capten/agent/pkg/sync/cluster_cliam.go
@@ -2,9 +2,12 @@ package sync
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"time"
 
+	"github.com/intelops/go-common/credentials"
 	"github.com/intelops/go-common/logging"
 	captenstore "github.com/kube-tarian/kad/capten/agent/pkg/capten-store"
 
@@ -15,10 +18,22 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
+var (
+	statusType        = "Ready"
+	statusValue       = "True"
+	clusterId         = "%s-%s"
+	clusterSecretName = "%s-cluster"
+	k8sCredEntity     = "kubernetesclusters"
+	kubeConfig        = "kubeconfig"
+	k8sEndpoint       = "endpoint"
+	k8sClusterCA      = "clusterCA"
+)
+
 type Fetch struct {
 	log    logging.Logger
 	client *k8s.K8SClient
 	db     *captenstore.Store
+	creds  credentials.CredentialAdmin
 }
 
 func NewFetch() (*Fetch, error) {
@@ -35,7 +50,13 @@ func NewFetch() (*Fetch, error) {
 		return nil, fmt.Errorf("failed to initalize k8s client: %v", err)
 	}
 
-	return &Fetch{log: log, client: k8sclient, db: db}, nil
+	credAdmin, err := credentials.NewCredentialAdmin(context.TODO())
+	if err != nil {
+		log.Audit("security", "storecred", "failed", "system", "failed to intialize credentials client")
+		return nil, err
+	}
+
+	return &Fetch{log: log, client: k8sclient, db: db, creds: credAdmin}, nil
 }
 
 // Run ...
@@ -76,15 +97,64 @@ func (fetch *Fetch) Run() {
 
 func (fetch *Fetch) UpdateClusterDetails(clObj []model.ClusterClaim) error {
 	for _, obj := range clObj {
-		managedCluster := &pb.ManagedCluster{}
-		managedCluster.Id = obj.Spec.Id
-		// add code to fetch the kubeconfig file
-		//updated the cluster name and other details
-		// err := fetch.db.UpsertManagedCluster(managedCluster)
-		// if err != nil {
-		// 	return err
-		// }
-	}
+		for _, status := range obj.Status.Conditions {
+			if status.Type != statusType {
+				continue
+			}
 
+			if status.Status != statusValue {
+				fetch.log.Info("%s in namespace %s, status is %s, so skiping update to db.",
+					obj.Spec.Id, obj.Metadata.Namespace, status.Status)
+				continue
+			}
+
+			managedCluster := &pb.ManagedCluster{}
+			managedCluster.Id = fmt.Sprintf(clusterId, obj.Spec.Id, obj.Metadata.Namespace)
+			managedCluster.ClusterName = obj.Spec.Id
+			managedCluster.ClusterDeployStatus = status.Status
+
+			// get the cluster endpoint and kubeconfig file from the secrets
+			req := &k8s.SecretDetailsRequest{Namespace: obj.Metadata.Namespace,
+				SecretName: fmt.Sprintf(clusterSecretName, obj.Spec.Id)}
+			resp, err := fetch.client.FetchSecretDetails(req)
+			if err != nil {
+				fetch.log.Info("%s in namespace %s, failed to get secret: %v",
+					req.SecretName, req.Namespace, err)
+				continue
+			}
+
+			clusterDetails := map[string]string{}
+			clusterDetails[kubeConfig] = resp.Data[kubeConfig]
+			clusterDetails[k8sClusterCA] = resp.Data[k8sClusterCA]
+
+			err = fetch.creds.PutCredential(context.TODO(), credentials.GenericCredentialType, k8sCredEntity, managedCluster.Id, clusterDetails)
+
+			if err != nil {
+				fetch.log.Audit("security", "storecred", "failed", "system", "failed to store crendential for %s", managedCluster.Id)
+				fetch.log.Errorf("failed to store credential for %s, %v", managedCluster.Id, err)
+				return err
+			}
+
+			managedCluster.ClusterEndpoint, err = getBase64DecodedString(resp.Data[k8sEndpoint])
+			if err != nil {
+				return fmt.Errorf("failed to decode base64 value: %v", err)
+			}
+
+			managedCluster.LastUpdateTime = time.Now().Format(time.RFC3339)
+			err = fetch.db.UpsertManagedCluster(managedCluster)
+			if err != nil {
+				return fmt.Errorf("failed to update information to db: %v", err)
+			}
+		}
+
+	}
 	return nil
+}
+
+func getBase64DecodedString(encodedString string) (string, error) {
+	decodedByte, err := base64.StdEncoding.DecodeString(encodedString)
+	if err != nil {
+		return "", err
+	}
+	return string(decodedByte), nil
 }
