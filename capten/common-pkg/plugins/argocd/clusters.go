8832a732c1e6124037b8b51643bbecbbec1b93bc
anil-sarodh <anil@sarodh.in>
2023-11-25 13:17:23 +0530

    minor changes

--- capten/common-pkg/plugins/argocd/clusters.go
+++ capten/common-pkg/plugins/argocd/clusters.go
@@ -2,8 +2,10 @@ package argocd
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/argoproj/argo-cd/v2/pkg/apiclient/cluster"
 	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
@@ -16,7 +18,7 @@ const (
 	CredIdentifier = "kubeconfig"
 )
 
-func (a *ArgoCDClient) CreateCluster(ctx context.Context, clusterReq *Cluster) (*v1alpha1.Cluster, error) {
+func (a *ArgoCDClient) CreateCluster(ctx context.Context, serverName string, config map[string]string) (*v1alpha1.Cluster, error) {
 	cred, err := credential.GetGenericCredential(ctx, CredEntityName, CredIdentifier)
 	if err != nil {
 		fmt.Println("Error occured while fetching kubeconfig")
@@ -36,33 +38,37 @@ func (a *ArgoCDClient) CreateCluster(ctx context.Context, clusterReq *Cluster) (
 	fmt.Println("byteConfig =>", string(byteConfig))
 	fmt.Println("Config =>", kubeconfig)
 
-	fmt.Println("clusterReq.Server" + kubeconfig.Clusters[0].Name)
+	fmt.Println("clusterReq.Server" + kubeconfig.Clusters[0].Cluster.Server)
 
-	if false {
-		conn, appClient, err := a.client.NewClusterClient()
-		if err != nil {
-			return nil, err
-		}
-		defer io.Close(conn)
+	conn, appClient, err := a.client.NewClusterClient()
+	if err != nil {
+		return nil, err
+	}
+	defer io.Close(conn)
 
-		resp, err := appClient.Create(ctx, &cluster.ClusterCreateRequest{
-			Cluster: &v1alpha1.Cluster{
-				Server: clusterReq.Server,
-				Name:   clusterReq.Name,
-				Config: v1alpha1.ClusterConfig{
-					BearerToken: clusterReq.Config.BearerToken,
-					TLSClientConfig: v1alpha1.TLSClientConfig{
-						ServerName: clusterReq.Config.ServerName,
-						CAData:     clusterReq.Config.CAData,
-					},
+	caData, err := base64.StdEncoding.DecodeString(strings.TrimSpace(kubeconfig.Clusters[0].Cluster.CertificateAuthorityData))
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := appClient.Create(ctx, &cluster.ClusterCreateRequest{
+		Cluster: &v1alpha1.Cluster{
+			Server: kubeconfig.Clusters[0].Cluster.Server,
+			Name:   kubeconfig.Contexts[0].Context.Cluster,
+			Config: v1alpha1.ClusterConfig{
+				BearerToken: kubeconfig.Users[0].User.ClientCertificateData,
+				TLSClientConfig: v1alpha1.TLSClientConfig{
+					ServerName: serverName,
+					CAData:     caData,
 				},
 			},
-		})
-		if err != nil {
-			return nil, err
-		}
-		return resp, nil
+		},
+	})
+	if err != nil {
+		return nil, err
 	}
+	return resp, nil
+
 	return nil, nil
 }
 
