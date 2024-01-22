117d23275c429bc2ce940f952407ccbf31812e29
indresh-28 <indresh@intelops.dev>
2024-01-22 20:33:52 +0530

    update the chnaged

--- capten/config-worker/internal/tekton/config_tekton_app.go
+++ capten/config-worker/internal/tekton/config_tekton_app.go
@@ -329,6 +329,7 @@ func (cp *TektonApp) createOrUpdateSecrets(ctx context.Context, req *model.Tekto
 			if err != nil {
 				return fmt.Errorf("failed to get argo-cd secret, %v", err)
 			}
+			log.Info("DATA : ", cfg)
 			strdata["SERVER_URL"] = []byte(cfg.ServiceURL)
 			strdata["USERNAME"] = []byte(cfg.Username)
 			strdata["PASSWORD"] = []byte(cfg.Password)
