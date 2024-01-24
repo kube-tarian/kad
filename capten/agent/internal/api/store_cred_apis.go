cee6ea0dab8ea8c20be0eddb99c5c7a6ba3f6b71
ShifnaZarnaz <shifna@intelops.dev>
2024-01-24 20:15:01 +0530

    Configure Vault Secret to K8S secret on capten cluster

--- capten/agent/internal/api/store_cred_apis.go
+++ capten/agent/internal/api/store_cred_apis.go
@@ -3,10 +3,8 @@ package api
 import (
 	"context"
 	"fmt"
-	"log"
 
-	//logger "github.com/kube-tarian/kad/integrator/common-pkg/logging"
-	managedcluster "github.com/kube-tarian/kad/capten/common-pkg/managed-cluster"
+	"github.com/kube-tarian/kad/capten/common-pkg/k8s"
 	vaultcred "github.com/kube-tarian/kad/capten/common-pkg/vault-cred"
 	v1 "k8s.io/api/core/v1"
 
@@ -17,11 +15,8 @@ import (
 )
 
 var (
-	clusterCredVaultPaths = map[string]string{"NATS": "generic/nats/auth-token"}
-	//natsNameSpace         = "observability"
-	//vaultAppRoleToken     = "vault-capten-token"
-	vaultAddress      = "http://vault.%s"
-	cluserAppRoleName = "approle-%s"
+	vaultAddress     = "http://vault.%s"
+	kadAppRolePrefix = "kad-approle-"
 )
 
 func (a *Agent) StoreCredential(ctx context.Context, request *agentpb.StoreCredentialRequest) (*agentpb.StoreCredentialResponse, error) {
@@ -72,53 +67,54 @@ func (a *Agent) GetClusterGlobalValues(ctx context.Context, _ *agentpb.GetCluste
 }
 
 func (a *Agent) ConfigureVaultSecret(ctx context.Context, request *agentpb.ConfigureVaultSecretRequest) (*agentpb.ConfigureVaultSecretResponse, error) {
-	// return nil, fmt.Errorf("not supported")
-	credentialPaths := []string{request.SecretPath}
+	a.log.Infof("Configure Vault Secret Request recieved for secret ", request.SecretName)
 
-	// logger.Logger.Infof("Cluster Id",request.ManagedClusterId)
-	clusterName := request.ManagedClusterId
-	log.Println("Cluster Id", request.ManagedClusterId)
-	cluserAppRoleNameStr := fmt.Sprintf(cluserAppRoleName, clusterName)
-	token, err := vaultcred.GetAppRoleToken(cluserAppRoleNameStr, credentialPaths)
-	log.Println("App role token", token)
-	if err != nil {
-		fmt.Errorf("failed to  get app role token, %v", err)
-
-		return &agentpb.ConfigureVaultSecretResponse{}, err
+	secretPathsData := map[string]string{}
+	secretPaths := []string{}
+	for _, secretPathData := range request.SecretPathData {
+		secretPathsData[secretPathData.SecretKey] = secretPathData.SecretPath
+		secretPaths = append(secretPaths, secretPathData.SecretPath)
 	}
-	log.Printf("approle token created for cluster %s", request.ManagedClusterId)
 
-	k8sclient, err := managedcluster.GetClusterK8SClient(ctx, request.ManagedClusterId)
+	appRoleName := kadAppRolePrefix + request.SecretName
+	token, err := vaultcred.GetAppRoleToken(appRoleName, secretPaths)
 	if err != nil {
-		fmt.Errorf("failed to initalize k8s client, %v", err)
-		return &agentpb.ConfigureVaultSecretResponse{}, err
+		a.log.Errorf("failed to create app role token for %s, %v", appRoleName, err)
+		return &agentpb.ConfigureVaultSecretResponse{Status: agentpb.StatusCode_INTERNRAL_ERROR}, err
+	}
+
+	k8sclient, err := k8s.NewK8SClient(a.log)
+	if err != nil {
+		a.log.Errorf("failed to initalize k8s client, %v", err)
+		return &agentpb.ConfigureVaultSecretResponse{Status: agentpb.StatusCode_INTERNRAL_ERROR}, err
 	}
 
 	cred := map[string][]byte{"token": []byte(token)}
-	err = k8sclient.CreateOrUpdateSecret(ctx, request.Namespace, request.SecretName, v1.SecretTypeOpaque, cred, nil)
+	vaultTokenSecretName := "vault-token-" + request.SecretName
+	err = k8sclient.CreateOrUpdateSecret(ctx, request.Namespace, vaultTokenSecretName, v1.SecretTypeOpaque, cred, nil)
 	if err != nil {
-		fmt.Errorf("failed to create cluter vault token secret, %v", err)
-		return &agentpb.ConfigureVaultSecretResponse{}, err
+		a.log.Errorf("failed to create cluter vault token secret, %v", err)
+		return &agentpb.ConfigureVaultSecretResponse{Status: agentpb.StatusCode_INTERNRAL_ERROR}, err
 	}
-	log.Printf("Secret %v created in the ns %v", request.SecretName, request.Namespace)
-	vaultAddressStr := fmt.Sprintf(vaultAddress, request.VaultDomainName)
-	//vaultAddressStr := "http://capten-dev-vault.platform.svc.cluster.local:8200"
-	vaultStoreCRData := fmt.Sprintf(vaultStore, request.Namespace, vaultAddressStr, request.SecretName)
-	ns, resource, err := k8sclient.DynamicClient.CreateResource(ctx, []byte(vaultStoreCRData))
-	if err != nil {
-		fmt.Errorf("failed to create cluter vault token secret, %v", err)
-		return &agentpb.ConfigureVaultSecretResponse{}, err
-	}
-	log.Printf("create %s on cluster cluster %s/%s", ns, resource, clusterName)
 
-	natsVaultExternalSecretData := fmt.Sprintf(natsVaultExternalSecret, request.Namespace, clusterCredVaultPaths["NATS"])
-	ns, resource, err = k8sclient.DynamicClient.CreateResource(ctx, []byte(natsVaultExternalSecretData))
+	vaultAddressStr := fmt.Sprintf(vaultAddress, a.cfg.DomainName)
+	secretStoreName := "ext-store-" + request.SecretName
+	err = k8sclient.CreateOrUpdateSecretStore(ctx, secretStoreName, request.Namespace, vaultAddressStr, vaultTokenSecretName, "token")
+	if err != nil {
+		a.log.Errorf("failed to create cluter vault token secret, %v", err)
+		return &agentpb.ConfigureVaultSecretResponse{Status: agentpb.StatusCode_INTERNRAL_ERROR}, err
+	}
+	a.log.Infof("created secret store %s/%s", request.Namespace, secretStoreName)
+
+	externalSecretName := "ext-secret-" + request.SecretName
+	err = k8sclient.CreateOrUpdateExternalSecret(ctx, externalSecretName, request.Namespace, secretStoreName,
+		request.SecretName, secretPathsData)
 	if err != nil {
 		fmt.Errorf("failed to create vault external secret, %v", err)
-		return &agentpb.ConfigureVaultSecretResponse{}, err
+		return &agentpb.ConfigureVaultSecretResponse{Status: agentpb.StatusCode_INTERNRAL_ERROR}, err
 	}
-	log.Printf("create %s on cluster cluster %s/%s", ns, resource, clusterName)
-	return &agentpb.ConfigureVaultSecretResponse{}, nil
+	a.log.Infof("created external secret %s/%s", request.Namespace, externalSecretName)
+	return &agentpb.ConfigureVaultSecretResponse{Status: agentpb.StatusCode_OK}, nil
 }
 
 func (a *Agent) CreateVaultRole(ctx context.Context, request *agentpb.CreateVaultRoleRequest) (*agentpb.CreateVaultRoleResponse, error) {
