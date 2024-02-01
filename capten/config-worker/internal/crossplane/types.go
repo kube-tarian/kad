f69633f04f60e3559d5bd3b6a1aab1aaa3c22cc5
anil-intelops <anil@intelops.dev>
2024-02-01 22:21:59 +0530

    modified the azure  provider config

--- capten/config-worker/internal/crossplane/types.go
+++ capten/config-worker/internal/crossplane/types.go
@@ -102,34 +102,34 @@ spec:
 
 const (
 	crossplaneAzureProviderTemplate = `
-apiVersion: pkg.crossplane.io/v1alpha1
-kind: ControllerConfig
-metadata:
-name: "%s-vault-config"
-spec:
-args:
-  - --debug
-metadata:
-  annotations:
-    vault.hashicorp.com/agent-inject: "true"
-    vault.hashicorp.com/role: "vault-role-crossplane"
-    vault.hashicorp.com/agent-inject-secret-creds.txt: "secret/%s"
-    vault.hashicorp.com/agent-inject-template-creds.txt: |
-      {{- with secret "secret/%s" -}}
-        [default]
-        AZURE_SUBSCRIPTION_ID="{{ .Data.data.subscriptionID }}"
-        AZURE_TENANT_ID="{{ .Data.data.tenantID }}"
-        AZURE_CLIENT_ID="{{ .Data.data.clientID }}"
-        AZURE_CLIENT_SECRET="{{ .Data.data.clientSecret }}"
-      {{- end -}}
----
-apiVersion: pkg.crossplane.io/v1
-kind: Provider
-metadata:
-name: provider-%s
-spec:
-package: "%s"
-controllerConfigRef:
-  name: "%s-vault-config"
+  apiVersion: pkg.crossplane.io/v1alpha1
+  kind: ControllerConfig
+  metadata:
+    name: "%s-vault-config"
+  spec:
+  args:
+    - --debug
+    metadata:
+      annotations:
+        vault.hashicorp.com/agent-inject: "true"
+        vault.hashicorp.com/role: "vault-role-crossplane"
+        vault.hashicorp.com/agent-inject-secret-creds.txt: "secret/%s"
+        vault.hashicorp.com/agent-inject-template-creds.txt: |
+          {{- with secret "secret/%s" -}}
+            [default]
+            AZURE_SUBSCRIPTION_ID="{{ .Data.data.subscriptionID }}"
+            AZURE_TENANT_ID="{{ .Data.data.tenantID }}"
+            AZURE_CLIENT_ID="{{ .Data.data.clientID }}"
+            AZURE_CLIENT_SECRET="{{ .Data.data.clientSecret }}"
+          {{- end -}}
+  ---
+  apiVersion: pkg.crossplane.io/v1
+  kind: Provider
+  metadata:
+    name: provider-%s
+  spec:
+    package: "%s"
+    controllerConfigRef:
+      name: "%s-vault-config"
 `
 )
