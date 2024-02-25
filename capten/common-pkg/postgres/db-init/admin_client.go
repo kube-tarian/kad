04b0abc7780669f469d656e402dd18a4965a9101
share2kanna <ganeshkl@intelops.dev>
2024-02-25 10:29:03 +0530

    capten sdk for DB setup and migrations added

---
+++ capten/common-pkg/postgres/db-init/admin_client.go
@@ -0,0 +1,100 @@
+package dbinit
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/intelops/go-common/logging"
+	"github.com/pkg/errors"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	createDatabaseQuery          = `CREATE DATABASE %s`
+	createUserQuery              = "CREATE USER %s WITH PASSWORD '%s' NOSUPERUSER;"
+	alterUserQuery               = "ALTER USER %s WITH PASSWORD '%s' NOSUPERUSER;"
+	grantPermissionDatabaseQuery = "GRANT ALL privileges ON database %s TO %s ;"
+	grantSchemaPublicQuery       = "GRANT ALL ON SCHEMA public TO %s;"
+	dsnTemplate                  = "postgres://%s:%s@%s:5432/"
+	dsnDBTemplate                = "postgres://%s:%s@%s:5432/%s"
+)
+
+type PostgresAdmin struct {
+	log     logging.Logger
+	session *gorm.DB
+}
+
+func NewPostgresAdmin(logger logging.Logger, dbAddr, dbAdminUsername, dbAdminPassword string) (*PostgresAdmin, error) {
+	pg := &PostgresAdmin{
+		log: logger,
+	}
+	err := pg.initSession(dbAddr, dbAdminUsername, dbAdminPassword)
+	if err != nil {
+		return nil, err
+	}
+	return pg, nil
+}
+
+func (p *PostgresAdmin) initSession(dbAddr, dbAdminUsername, dbAdminPassword string) (err error) {
+	// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable"
+	dsn := fmt.Sprintf(dsnTemplate, dbAdminUsername, dbAdminPassword, dbAddr)
+	p.session, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return
+}
+
+func (p *PostgresAdmin) Session() *gorm.DB {
+	return p.session
+}
+
+func (p *PostgresAdmin) CreateDbUser(serviceUsername string, servicePassword string) (err error) {
+	result := p.session.Exec(fmt.Sprintf(createUserQuery, serviceUsername, servicePassword))
+	if result.Error != nil {
+		if strings.Contains(result.Error.Error(), "already exists") {
+			return p.updateDbUser(serviceUsername, servicePassword)
+		} else {
+			err = errors.WithMessage(err, "failed to create service user")
+			return
+		}
+	}
+	return
+}
+
+func (p *PostgresAdmin) GrantPermission(serviceUsername string, dbAddr, dbAdminUsername, dbAdminPassword, dbName string) error {
+	dbDSN := fmt.Sprintf(dsnDBTemplate, dbAdminUsername, dbAdminPassword, dbAddr, dbName)
+	fmt.Println(dbDSN)
+	session, err := gorm.Open(postgres.Open(dbDSN), &gorm.Config{})
+	if err != nil {
+		return errors.WithMessage(err, "failed to grant permission to service user")
+	}
+
+	result := session.Exec(fmt.Sprintf(grantPermissionDatabaseQuery, dbName, serviceUsername))
+	if result.Error != nil {
+		return errors.WithMessage(result.Error, "failed to grant permission to service user")
+	}
+
+	result = session.Exec(fmt.Sprintf(grantSchemaPublicQuery, serviceUsername))
+	if result.Error != nil {
+		return errors.WithMessage(result.Error, "failed to grant permission to service user")
+	}
+	p.log.Infof("Grant permission successful")
+	return nil
+}
+
+func (p *PostgresAdmin) CreateDb(dbName string) (err error) {
+	result := p.session.Exec(fmt.Sprintf(createDatabaseQuery, dbName))
+	if result.Error != nil {
+		err = errors.WithMessage(err, "failed to create the Database")
+		return
+	}
+	return
+}
+
+func (c *PostgresAdmin) updateDbUser(serviceUsername string, servicePassword string) (err error) {
+	result := c.session.Exec(fmt.Sprintf(alterUserQuery, serviceUsername, servicePassword))
+	if result.Error != nil {
+		err = errors.WithMessage(result.Error, "failed to update service user")
+		return
+	}
+	return
+}
---
+++ capten/common-pkg/postgres/db-migrate/postgres/postgresql_migration.go
@@ -0,0 +1,83 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+
+	bindata "github.com/golang-migrate/migrate/v4/source/go_bindata"
+	"github.com/intelops/go-common/logging"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/kube-tarian/kad/capten/common-pkg/credential"
+	"github.com/kube-tarian/kad/capten/common-pkg/postgres/db-migrate/migration"
+	"github.com/pkg/errors"
+)
+
+type DBConfig struct {
+	DBAddr     string `envconfig:"PG_DB_ADDRESS" required:"true"`
+	EntityName string `envconfig:"PG_DB_ENTITY_NAME" required:"true"`
+	DBName     string `envconfig:"PG_DB_NAME" required:"true"`
+	Username   string `envconfig:"PG_DB_SERVICE_USERNAME" required:"true"`
+	Password   string `envconfig:"PG_DB_SERVICE_USER_PASSWORD" required:"false"`
+	SourceURI  string `envconfig:"PG_SOURCE_URI" default:"go-bindata"`
+}
+
+func RunMigrations(log logging.Logger, mode migration.Mode) error {
+	conf := &DBConfig{}
+	if err := envconfig.Process("", conf); err != nil {
+		return err
+	}
+
+	return RunMigrationsWithConfig(log, conf, mode)
+}
+
+func RunMigrationsWithConfig(log logging.Logger, conf *DBConfig, mode migration.Mode) error {
+	dbConnectionString, err := getDbConnectionURLFromDbType(conf, "")
+	if err != nil {
+		return errors.WithMessage(err, "DB connection Url create failed")
+	}
+
+	// file source based migrations
+	if err := migration.RunMigrations(conf.SourceURI, dbConnectionString, conf.DBName, mode); err != nil {
+		return err
+	}
+
+	log.Info("Migrations applied successfully")
+	return nil
+}
+
+func RunMigrationsBinDataWithConfig(log logging.Logger, conf *DBConfig, s *bindata.AssetSource, mode migration.Mode) error {
+	// TODO: password to be passed empty for production
+	dbConnectionString, err := getDbConnectionURLFromDbType(conf, conf.Password)
+	if err != nil {
+		return errors.WithMessage(err, "DB connection Url create failed")
+	}
+
+	if err := migration.RunMigrationsFromBinData(s, conf.SourceURI, dbConnectionString, conf.DBName, mode); err != nil {
+		return err
+	}
+
+	log.Info("Migrations applied with bindata successfully")
+	return nil
+}
+
+func getDbConnectionURLFromDbType(conf *DBConfig, password string) (string, error) {
+	if len(password) == 0 {
+		serviceCredential, err := credential.GetServiceUserCredential(context.Background(),
+			conf.EntityName, conf.Username)
+		if err != nil {
+			return "", err
+		}
+		// postgres://user:password@host:port/dbname?query
+		return fmt.Sprintf("postgres://%s:%s@%s:5432/%s",
+			conf.Username, url.QueryEscape(serviceCredential.Password), conf.DBAddr, conf.DBName), nil
+	}
+
+	// postgres://user:password@host:port/dbname?query
+	return fmt.Sprintf("postgres://%s:%s@%s:5432/%s?sslmode=disable",
+		conf.Username, password, conf.DBAddr, conf.DBName), nil
+}
+
+func GetResource(names []string, afn bindata.AssetFunc) *bindata.AssetSource {
+	return bindata.Resource(names, afn)
+}
