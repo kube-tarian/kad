986db085324006e4ca52351a828807cae808280c
anil-intelops <anil@intelops.dev>
2024-03-07 20:31:30 +0530

    minor changes

--- capten/agent/internal/postgres/postgres.go
+++ capten/agent/internal/postgres/postgres.go
@@ -1,8 +1,6 @@
 package postgres
 
 import (
-	"fmt"
-
 	"github.com/intelops/go-common/logging"
 	"github.com/kube-tarian/kad/capten/common-pkg/psql"
 	"gorm.io/gorm"
@@ -21,17 +19,6 @@ func NewPostgres(log logging.Logger) *Postgres {
 func (handler *Postgres) RunMigrations() error {
 
 	err := handler.db.AutoMigrate(GitProjects{}, CloudProviders{}, ContainerRegistry{})
-
-	return err
-}
-
-func (handler *Postgres) CreatedDatabase() error {
-
-	if err := psql.GetPostgresRootConnectionStatus(); err != nil {
-		return err
-	}
-
-	err := handler.db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s;", "capten")).Error
 
 	return err
 }
