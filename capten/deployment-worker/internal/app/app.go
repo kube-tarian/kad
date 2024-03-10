a833ed57593debc232a06dbabe2bdfc5736794e5
share2kanna <ganeshkl@intelops.dev>
2024-03-10 20:19:10 +0530

    Added support for multiple workflows and activities to worker framework

--- capten/deployment-worker/internal/app/app.go
+++ capten/deployment-worker/internal/app/app.go
@@ -15,10 +15,13 @@ func Start() {
 	logger := logging.NewLogger()
 	logger.Infof("Started deployment worker\n")
 
-	worker, err := workerframework.NewWorker(WorkflowTaskQueueName, workflows.Workflow, &activities.Activities{}, logger)
+	worker, err := workerframework.NewWorkerV2(WorkflowTaskQueueName, logger)
 	if err != nil {
 		logger.Fatalf("Worker initialization failed, Reason: %v\n", err)
 	}
+
+	worker.RegisterWorkflows([]interface{}{workflows.Workflow})
+	worker.RegisterActivities([]interface{}{&activities.Activities{}})
 
 	logger.Infof("Running deployment worker..\n")
 	if err := worker.Run(); err != nil {
