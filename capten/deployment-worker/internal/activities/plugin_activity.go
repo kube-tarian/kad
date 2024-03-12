5d1e29411b71b24224bd630bb4ddd5f4f2d75a2f
share2kanna <ganeshkl@intelops.dev>
2024-03-12 22:36:14 +0530

    cluster plugin proto is used for plugin workflow

--- capten/deployment-worker/internal/activities/plugin_activity.go
+++ capten/deployment-worker/internal/activities/plugin_activity.go
@@ -3,25 +3,49 @@ package activities
 import (
 	"context"
 
-	"github.com/kube-tarian/kad/capten/common-pkg/plugin_store/pluginstorepb"
+	"github.com/kube-tarian/kad/capten/common-pkg/cluster-plugins/clusterpluginspb"
+	dbstore "github.com/kube-tarian/kad/capten/deployment-worker/internal/db-store"
 )
 
 type PluginActivities struct {
-	state string
+	store *dbstore.Store
 }
 
-func NewPluginActivities() *PluginActivities {
-	return &PluginActivities{
-		state: "initialized",
+func NewPluginActivities() (*PluginActivities, error) {
+	store, err := dbstore.NewStore(logger)
+	if err != nil {
+		return nil, err
 	}
+
+	return &PluginActivities{
+		store: store,
+	}, nil
 }
 
-func (p *PluginActivities) PluginDeployActivity(ctx context.Context, req *pluginstorepb.DeployPluginRequest) (*pluginstorepb.DeployPluginResponse, error) {
-	logger.Infof("state: %v", p.state)
+func (p *PluginActivities) PluginDeployActivity(ctx context.Context, req *clusterpluginspb.DeployClusterPluginRequest) (*clusterpluginspb.DeployClusterPluginResponse, error) {
 	return nil, nil
 }
 
-func (p *PluginActivities) PluginUndeployActivity(ctx context.Context, req *pluginstorepb.DeployPluginRequest) (*pluginstorepb.DeployPluginResponse, error) {
-	logger.Infof("state: %v", p.state)
+func (p *PluginActivities) PluginUndeployActivity(ctx context.Context, req *clusterpluginspb.DeployClusterPluginRequest) (*clusterpluginspb.DeployClusterPluginResponse, error) {
+	return nil, nil
+}
+
+func (p *PluginActivities) PluginDeployPreActionPostgresStoreActivity(ctx context.Context, req *clusterpluginspb.DeployClusterPluginRequest) (*clusterpluginspb.DeployClusterPluginResponse, error) {
+	return nil, nil
+}
+
+func (p *PluginActivities) PluginDeployPreActionVaultStoreActivity(ctx context.Context, req *clusterpluginspb.DeployClusterPluginRequest) (*clusterpluginspb.DeployClusterPluginResponse, error) {
+	return nil, nil
+}
+
+func (p *PluginActivities) PluginDeployPreActionMTLSActivity(ctx context.Context, req *clusterpluginspb.DeployClusterPluginRequest) (*clusterpluginspb.DeployClusterPluginResponse, error) {
+	return nil, nil
+}
+
+func (p *PluginActivities) PluginDeployInstallActivity(ctx context.Context, req *clusterpluginspb.DeployClusterPluginRequest) (*clusterpluginspb.DeployClusterPluginResponse, error) {
+	return nil, nil
+}
+
+func (p *PluginActivities) PluginDeployPostActionActivity(ctx context.Context, req *clusterpluginspb.DeployClusterPluginRequest) (*clusterpluginspb.DeployClusterPluginResponse, error) {
 	return nil, nil
 }
