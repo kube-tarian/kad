59bc66c8749aaa0d48fb4d3cdabb1f6b2ed8e6a9
Venkatreddy KP <venkatreddy.kp@intelops.dev>
2024-03-23 13:58:37 +0530

    register crossplane k8s watcher when resource available

--- capten/agent/internal/crossplane/watchers.go
+++ capten/agent/internal/crossplane/watchers.go
@@ -2,6 +2,7 @@ package crossplane
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/intelops/go-common/logging"
 	captenstore "github.com/kube-tarian/kad/capten/agent/internal/capten-store"
@@ -14,14 +15,31 @@ func RegisterK8SWatcher(log logging.Logger, dbStore *captenstore.Store) error {
 		return fmt.Errorf("failed to initalize k8s client: %v", err)
 	}
 
-	err = registerK8SClusterClaimWatcher(log, dbStore, k8sclient.DynamicClientInterface)
-	if err != nil {
-		return fmt.Errorf("failed to RegisterK8SClusterClaimWatcher: %v", err)
-	}
+	go retryForEver(60*time.Second, func() (err error) {
+		err = registerK8SClusterClaimWatcher(log, dbStore, k8sclient)
+		if err != nil {
+			return fmt.Errorf("failed to RegisterK8SClusterClaimWatcher: %v", err)
+		}
+		return nil
+	})
 
-	err = registerK8SProviderWatcher(log, dbStore, k8sclient.DynamicClientInterface)
-	if err != nil {
-		return fmt.Errorf("failed to RegisterK8SProviderWatcher: %v", err)
-	}
+	go retryForEver(60*time.Second, func() (err error) {
+		err = registerK8SProviderWatcher(log, dbStore, k8sclient)
+		if err != nil {
+			return fmt.Errorf("failed to RegisterK8SProviderWatcher: %v", err)
+		}
+		return nil
+	})
 	return nil
+}
+
+func retryForEver(sleep time.Duration, f func() error) (err error) {
+	for i := 0; ; i++ {
+		err = f()
+		if err == nil {
+			return
+		}
+		time.Sleep(sleep)
+	}
+	return
 }
